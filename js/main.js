a3c8d081b9671dd9ca236b12e0db98ae536a3659
pmcintyre3 <pmcin3@uga.edu>
2015-04-09 05:04:16 -0400

    A* Works!
    Need to implement highlighting the best route and possibly a menu page.
    But the basic algorithm works!

--- js/main.js
+++ js/main.js
@@ -25,10 +25,9 @@ var grid = [];
 
 var isDone = false;
 
-
 var mx, my;
 
-window.addEventListener("keydown", keyFunc, false);
+var winList = [];
 
 document.onmousemove = function(e){
 	mouseX = e.pageX - (window.innerWidth - width) / 2;
@@ -43,17 +42,18 @@ document.onmousedown = function(e){
 	var mouseCol = Math.floor(mouseX / (nDim + nBrd));
 	var mouseRow = Math.floor(mouseY / (nDim + nBrd));
 	
-	console.log("mouseCol: " + mouseCol + ", mouseRow: " + mouseRow);
+	// console.log("mouseCol: " + mouseCol + ", mouseRow: " + mouseRow);
 	
 	if(!e)
 		e = window.event;
 	
 	var block = grid[mouseRow][mouseCol];
 	
-	// if(!block.path){
+	if(!block.path){
 		if(block.isStart){
 			block.isStart = false;
 			block.isObs = false;
+			start = null;
 			startExist = false;
 			
 			if(endExist){
@@ -63,11 +63,14 @@ document.onmousedown = function(e){
 				block.isEnd = true;
 				endX = block.x;
 				endY = block.y;
+				end = block;
+				endExist = true;
 			}
 		}
 		else if(block.isEnd){
 			block.isEnd = false;
 			block.isObs = false;
+			end = null;
 			endExist = false;
 		}
 		else{
@@ -87,6 +90,7 @@ document.onmousedown = function(e){
 					block.isEnd = true;
 					block.isObs = false;
 					
+					end = block;
 					endX = block.x;
 					endY = block.y;
 					endExist = true;
@@ -96,84 +100,31 @@ document.onmousedown = function(e){
 					block.isEnd = false;
 					block.isObs = false;
 					
+					start = block;
 					startX = block.x;
 					startY = block.y;
 					startExist = true;
 				}
 			}
 		}
-	// }
+	}
+	
+	draw();
 }
 
-// function getClickPosition(e) {
-    // var parentPosition = getPosition(e.currentTarget);
-    // var mx = e.clientX - parentPosition.x;
-    // var my = e.clientY - parentPosition.y;
-	
-	// return {x: xPosition, y: yPosition};
-// }
- 
-// function getPosition(element) {
-    // var xPosition = 0;
-    // var yPosition = 0;
-      
-    // while (element) {
-        // xPosition += (element.offsetLeft - element.scrollLeft + element.clientLeft);
-        // yPosition += (element.offsetTop - element.scrollTop + element.clientTop);
-        // element = element.offsetParent;
-    // }
-    // return { x: xPosition, y: yPosition };
-// }
-
-// HTMLCanvasElement.prototype.getMouse = function(e) {
-  // var element = this, offsetX = 0, offsetY = 0, mx, my;
-
-  // if (element.offsetParent !== undefined) {
-    // do {
-      // offsetX += element.offsetLeft;
-      // offsetY += element.offsetTop;
-    // } while ((element = element.offsetParent));
-  // }
-
-  // offsetX += this.stylePaddingLeft + this.styleBorderLeft + this.htmlLeft;
-  // offsetY += this.stylePaddingTop + this.styleBorderTop + this.htmlTop;
-
-  // mx = e.pageX - offsetX;
-  // my = e.pageY - offsetY;
-
-  // console.log("mx: " + mx + ", my: " + my);
-  
-  // return {x: mx, y: my};
-// }
-
-// canvas.addEventListener("mousedown", getPosition, false);
-
-// function getPosition(event)
-// {
-  // var mx = event.x;
-  // var my = event.y;
-
-  // console.log(event.x);
-  
-  // // var canvas = document.getElementById("canvas");
-
-  // mx -= canvas.offsetLeft;
-  // my -= canvas.offsetTop;
-
-  // console.log("mx: " + mx + ", my: " + my);
-  
-  // return {x: mx, y: my};
-// }
-
 function keyFunc(e){
-	switch(e.keycode){
+	switch(e.keyCode){
 		case 32:
-		if(!isDone)
-			A_star();
-		else
-			init();
-		isDone = !isDone;
-		break;
+			if(isDone){
+				console.log("init...");
+				init();
+			}
+			else{
+				console.log("A_Star...");
+				A_star();
+			}
+			isDone = !isDone;
+			break;
 	}
 }
 
@@ -185,8 +136,13 @@ node = function(){
 	this.x;
 	this.y;
 	
-	this.parent;
+	this.f = 0;
+	this.g = 0;
+	this.h = 0;
+	
+	this.parent = 0;
 	this.path = false;
+	this.winPath = false;
 	
 	this.neighbors = new Array();
 }
@@ -197,6 +153,7 @@ function A_star(){
 		for(var j = 0; j < cols; j++){
 			if(grid[i][j].isStart){
 				start = grid[i][j];
+				// console.log("start: " + j + "," + i);
 			}
 			if(grid[i][j].isEnd){
 				end = grid[i][j];
@@ -207,60 +164,167 @@ function A_star(){
 	var closedset = new Array();
 	var openset = new Array();
 	
+	start.g = 0;
+	manhattanDistance(start);
+	start.f = start.g + start.h;
+	start.parent = null;
+	
 	openset.push(start);
 	
+	
 	while(openset.length > 0){
-		var current;
-		var min;
-		var minIndex;
 		
-		for(var i = 0; i < openset.length; i++){
-			if(i == 0){
-				min = openset[0].f;
-				minIndex = 0;
-			}
-			else{
-				if(openset[i].f < min){
-					min = openset[i].f;
-					minIndex = i;
-				}
+		var current;
+
+		var min = Number.POSITIVE_INFINITY;
+		var minIndex = 0;
+		
+		for(var i = openset.length-1; i >= 0; i--){
+			if(openset[i].f < min){
+				min = openset[i].f;
+				minIndex = i;
 			}
 		}
 		
-		current = openset[i];
-		openset.splice(myIndex, 1);
+		current = openset[minIndex];
+		
+		if(current.isEnd){
+			
+			current.winPath = true;
+			end.parent = current;
+			
+			return followPath(end);
+		}
+		
+		current.path = true;
+		
+		openset.splice(minIndex, 1);
 		closedset.push(current);
 		
-		if(current.isEnd)
-			return followPath(visited, end);
 		
-		var neighbors = findNeighbors(current);
+		findNeighbors(current);
+		
+		var neighbors = current.neighbors;
 		
 		for(var j = 0; j < neighbors.length; j++){
-			if(closedset.indexOf(neighbor[j]) < 0){
+			if(closedset.indexOf(neighbors[j]) > 0 || neighbors[j].isObs){
 				continue;
 			}
 			
 			var temp_g;
 			
-			if(current.x == neighbor[j].x || current.y == neighbor[j].y){
+			if(current.x == neighbors[j].x || current.y == neighbors[j].y){
 				temp_g = current.g + 10;
 			}
 			else{
 				temp_g = current.g + 14;
 			}
 			
-			if(openset.indexOf(neighbor[j]) < 0 || temp_g < neighbor[j].g){
-				neighbor[j].parent = current;
-				neighbor[j].g = temp_g;
-				neighbor[j].f = neighbor[j].g + manhattanDistance(neighbor[j]);
+			if(openset.indexOf(neighbors[j]) < 0 ||temp_g < neighbors[j].g){
+				neighbors[j].parent = current;
+				neighbors[j].g = temp_g;
+				manhattanDistance(neighbors[j]);
+				neighbors[j].f = neighbors[j].g + neighbors[j].h;
 				
-				if(openset.indexOf(neighbor[j]) < 0){
-					openset.push(neighbor[j]);
+				if(openset.indexOf(neighbors[j]) < 0){
+					openset.push(neighbors[j]);
 				}
 			}
 		}
+		draw();
 	}
+	alert("No path found!");
+	init();
+}
+
+function followPath(curr){
+	var p = curr;
+	p.winPath = true;
+	
+	// while(p.parent != start){
+		// p.winPath = true;
+		// p.path = false;
+		// p = p.parent;
+		// draw();
+	// }
+	
+	// if(p.parent == start)
+		// return;
+	// else{
+		// p = p.parent;
+		// return followPath(p);
+	// }
+	
+	draw();
+	
+}
+
+Array.prototype.min = function() {
+  return Math.min.apply(null, this.f);
+};
+
+function manhattanDistance(node){
+	
+	if(end){
+		var dx = Math.floor(Math.abs(node.x - endX));
+		var dy = Math.floor(Math.abs(node.y - endY));
+	
+		node.h = ((dx + dy) * 10);
+	}
+	else
+		alert("H undefined!");
+}
+
+function findNeighbors(node){
+	var nodeNeighbors = [];
+	var x = node.x;
+	var y = node.y;
+	
+	//up, down, right, left
+	if(grid[y - 1] && grid[y-1][x]){
+		if(!grid[y-1][x].isObs)
+			nodeNeighbors.push(grid[y-1][x]);
+	}
+	if(grid[y + 1] && grid[y + 1][x])
+		if(!grid[y + 1][x].isObs)
+			nodeNeighbors.push(grid[y + 1][x]);
+	if(grid[y][x + 1] && grid[y][x + 1])
+		if(!grid[y][x + 1].isObs){
+			nodeNeighbors.push(grid[y][x + 1]);
+		}
+	if(grid[y][x - 1] && grid[y][x - 1])
+		if(!grid[y][x - 1].isObs){
+			nodeNeighbors.push(grid[y][x - 1]);
+		}
+	
+	//diagonals
+	
+	if(grid[y - 1] && grid[y - 1][x - 1]){
+		if(!grid[y - 1][x - 1].isObs && !grid[y][x - 1].isObs && !grid[y - 1][x].isObs){
+			nodeNeighbors.push(grid[y - 1][x - 1]);
+			grid[y - 1][x - 1].cost = 14;
+		}
+	}
+	if(grid[y - 1] && grid[y - 1][x + 1]){
+		if(!grid[y - 1][x + 1].isObs && !grid[y][x + 1].isObs && !grid[y - 1][x].isObs){
+			nodeNeighbors.push(grid[y - 1][x + 1]);
+			grid[y - 1][x + 1].cost = 14;
+		}
+	}
+	if(grid[y + 1] && grid[y + 1][x - 1]){
+		if(!grid[y + 1][x - 1].isObs && !grid[y][x - 1].isObs && !grid[y + 1][x].isObs){
+			nodeNeighbors.push(grid[y + 1][x - 1]);
+			grid[y + 1][x - 1].cost = 14;
+		}
+	}
+	if(grid[y + 1] && grid[y + 1][x + 1]){
+		if(!grid[y + 1][x + 1].isObs && !grid[y][x + 1].isObs && !grid[y + 1][x].isObs){
+			nodeNeighbors.push(grid[y + 1][x + 1]);
+			grid[y + 1][x + 1].cost = 14;
+		}
+	}
+	
+	node.neighbors = nodeNeighbors;
 }
 
 function init(){
@@ -271,7 +335,7 @@ function init(){
 	startY = 2;
 	
 	endX = 8;
-	endY = 8;
+	endY = 7;
 	
 	nBrd = 1;
 	nDim =(width - ((rows - 1) * nBrd)) / rows;
@@ -289,12 +353,14 @@ function init(){
 			t.y = i;
 		
 			if(i == startY && j == startX){
+				start = t;
 				t.isStart = true;
 				t.isEnd = false;
 				t.isObs = false;
 				startExist = true;
 			}
 			else if(i == endY && j == endX){
+				end = t;
 				t.isStart = false;
 				t.isEnd = true;
 				t.isObs = false;
@@ -314,34 +380,40 @@ function init(){
 }
 
 function draw(){
+	
+	context.clearRect(0,0,width, height);
+	
 	for(var i = 0; i < rows; i++){
 		for(var j = 0; j < cols; j++){
 			var block = grid[i][j];
+			
 			var str = "";
-			
-			if(block.path == false){
-				if(block.Obs)
-					str = "green";
-				else if(block.isStart)
-					str = "blue";
-				else if(block.isEnd)
-					str = "red";
-				else
-					str = "black";
-			}
-			else{
-				if(block.isStart)
-					str = "blue";
-				else if(block.isEnd)
-					str = "red";
-				else
+				if(block.winPath)
 					str = "yellow";
-			}
-			
+				else{
+					// if(block.winPath && (!block.isStart || !block.isEnd) && !block.path)
+						// str = "yellow";
+					if(block.isStart)
+						str = "blue";
+					else if(block.isEnd)
+						str = "red";
+					else if(block.isObs)
+						str = "green";
+					else if(block.path && (!block.isStart || !block.isEnd))
+						str = "#232323";
+					else
+						str = "black";
+				}
+				
 			context.fillStyle = str;
-			context.fillRect(i * (nDim + nBrd), j * (nDim + nBrd), nDim, nDim);
+			context.fillRect(j * (nDim + nBrd), i * (nDim + nBrd), nDim, nDim);
+			
+			context.font="7px Helvetica";
+			context.fillStyle = "#ffffff";
+			context.fillText(grid[i][j].g,(j * (nDim + nBrd)) + 2, (i * (nDim + nBrd)) + 10);
 		}
 	}
 }
 
 init();
+window.addEventListener("keyup", keyFunc, false);
